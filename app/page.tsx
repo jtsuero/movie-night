8bea80535bd78e5c905436dafc703c99086d912f
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2023-04-29 15:03:58 -0700

    add skeleton search ability to home page for api build

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,41 @@
+"use client";
+import { searchMoviesByTitle, Movie } from "./api/movies/route";
+
+// export const metadata = {
+// 	title: "Movie Night",
+// 	description: "What you watching?",
+// };
+
+import { useState } from "react";
+
 export default function Home() {
+	const [searchQuery, setSearchQuery] = useState("");
+	const [searchResults, setSearchResults] = useState<Movie[]>([]);
+
+	const handleSearch = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		const results = await searchMoviesByTitle(searchQuery);
+		console.log(results, "results");
+		setSearchResults(results);
+	};
+
 	return (
-		<main>
-			<div>Hello, World! 👋</div>
-		</main>
+		<>
+			<form onSubmit={handleSearch}>
+				<input
+					type='text'
+					value={searchQuery}
+					onChange={(event) => setSearchQuery(event.target.value)}
+				/>
+				<button type='submit'>Search</button>
+			</form>
+
+			<ul>
+				{searchResults &&
+					searchResults.map((movie) => (
+						<li key={movie.imdbID}>{movie.Title}</li>
+					))}
+			</ul>
+		</>
 	);
 }

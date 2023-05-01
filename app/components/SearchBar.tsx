ab14dc65344dbc606ad6f0817b4fdbd9d5c7ad32
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2023-04-30 21:45:57 -0700

    remove unused code

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -9,13 +9,6 @@ interface SearchBarProps {
 export default function SearchBar(props: SearchBarProps) {
 	return (
 		<>
-			<Head>
-				<link
-					href='https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap'
-					rel='stylesheet'
-				/>
-			</Head>
-
 			<Container maxW={"3xl"}>
 				<Stack
 					as={Box}
@@ -23,16 +16,7 @@ export default function SearchBar(props: SearchBarProps) {
 					spacing={{ base: 8, md: 14 }}
 					py={{ base: 20, md: 36 }}
 				>
-					<Heading
-						fontWeight={600}
-						fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
-						lineHeight={"110%"}
-					>
-						<Text as={"span"} color={"red.700"}>
-							Movie Night
-						</Text>
-					</Heading>
-					<Text color={"gray.500"}>
+					<Text fontSize={25} fontFamily={"monospace"}>
 						Find tonight's movie or bookmark it for later!
 					</Text>
 					<Form onSubmit={props.handleSearch} />

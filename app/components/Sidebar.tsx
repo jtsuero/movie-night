0cf76e8a206478fe4dcc532b11c1e1bcec545b68
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2023-04-30 21:15:22 -0700

    create sidebar

---
+++ app/components/Sidebar.tsx
@@ -0,0 +1,162 @@
+import {
+	Box,
+	BoxProps,
+	CloseButton,
+	Drawer,
+	DrawerContent,
+	Flex,
+	FlexProps,
+	Icon,
+	IconButton,
+	Link,
+	Text,
+	useColorModeValue,
+	useDisclosure,
+} from "@chakra-ui/react";
+import { ReactNode, ReactText } from "react";
+import { IconType } from "react-icons";
+import { BsBookmark } from "react-icons/bs";
+import { FiHome, FiMenu } from "react-icons/fi";
+import { RiEyeLine } from "react-icons/ri";
+
+interface LinkItemProps {
+	name: string;
+	icon: IconType;
+	href: string;
+}
+const LinkItems: Array<LinkItemProps> = [
+	{ name: "Home", icon: FiHome, href: "/" },
+	{ name: "Bookmarks", icon: BsBookmark, href: "/bookmarks" },
+	{ name: "Watched", icon: RiEyeLine, href: "/watched" },
+];
+
+export default function Sidebar({ children }: { children: ReactNode }) {
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	return (
+		<Box minH='100vh' bg={"white"}>
+			<SidebarContent
+				onClose={() => onClose}
+				display={{ base: "none", md: "block" }}
+			/>
+			<Drawer
+				autoFocus={false}
+				isOpen={isOpen}
+				placement='left'
+				onClose={onClose}
+				returnFocusOnClose={false}
+				onOverlayClick={onClose}
+				size='full'
+			>
+				<DrawerContent>
+					<SidebarContent onClose={onClose} />
+				</DrawerContent>
+			</Drawer>
+			{/* mobilenav */}
+			<MobileNav display={{ base: "flex", md: "none" }} onOpen={onOpen} />
+			<Box ml={{ base: 0, md: 60 }} p='4'>
+				{children}
+			</Box>
+		</Box>
+	);
+}
+
+interface SidebarProps extends BoxProps {
+	onClose: () => void;
+}
+
+const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
+	return (
+		<Box
+			bg={"white"}
+			borderRight='1px'
+			borderRightColor={useColorModeValue("gray.200", "gray.700")}
+			w={{ base: "full", md: 60 }}
+			pos='fixed'
+			h='full'
+			{...rest}
+		>
+			<Flex h='20' alignItems='center' mx='8' justifyContent='space-between'>
+				<Text fontSize='2xl' fontFamily='monospace' fontWeight='bold'>
+					MovieNight
+				</Text>
+				<CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
+			</Flex>
+			{LinkItems.map((link) => (
+				<NavItem key={link.name} icon={link.icon} href={link.href}>
+					{link.name}
+				</NavItem>
+			))}
+		</Box>
+	);
+};
+
+interface NavItemProps extends FlexProps {
+	icon: IconType;
+	href: string;
+	children: ReactText;
+}
+const NavItem = ({ icon, children, href, ...rest }: NavItemProps) => {
+	return (
+		<Link
+			href={href}
+			style={{ textDecoration: "none" }}
+			_focus={{ boxShadow: "none" }}
+		>
+			<Flex
+				align='center'
+				p='4'
+				mx='4'
+				borderRadius='lg'
+				role='group'
+				cursor='pointer'
+				_hover={{
+					bg: "blue.400",
+					color: "white",
+				}}
+				{...rest}
+			>
+				{icon && (
+					<Icon
+						mr='4'
+						fontSize='16'
+						_groupHover={{
+							color: "white",
+						}}
+						as={icon}
+					/>
+				)}
+				{children}
+			</Flex>
+		</Link>
+	);
+};
+
+interface MobileProps extends FlexProps {
+	onOpen: () => void;
+}
+const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
+	return (
+		<Flex
+			ml={{ base: 0, md: 60 }}
+			px={{ base: 4, md: 24 }}
+			height='20'
+			alignItems='center'
+			bg={"white"}
+			borderBottomWidth='1px'
+			borderBottomColor={useColorModeValue("gray.200", "gray.700")}
+			justifyContent='flex-start'
+			{...rest}
+		>
+			<IconButton
+				variant='outline'
+				onClick={onOpen}
+				aria-label='open menu'
+				icon={<FiMenu />}
+			/>
+
+			<Text fontSize='2xl' ml='8' fontFamily='monospace' fontWeight='bold'>
+				MovieNight
+			</Text>
+		</Flex>
+	);
+};

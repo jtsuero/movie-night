bc67646e67fbf5b1c450ef3b165c7b5fa05490ee
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2023-04-30 22:46:34 -0700

    add watched page

---
+++ app/watched/page.tsx
@@ -0,0 +1,96 @@
+"use client";
+import { Box, Button, Heading, Link, Stack, Text } from "@chakra-ui/react";
+import { Movie } from "../api/movies/app/api/search/[title]";
+import MovieCard from "../components/MovieCard";
+import { useMovieManager } from "../store";
+
+export default function Watched() {
+	const [bookmarkedMovies, addBookmarkedMovie, removeBookmarkedMovie] =
+		useMovieManager("bookmarkedMovies");
+	const [watchedMovies, addWatchedMovie, removeWatchedMovie] =
+		useMovieManager("watchedMovies");
+
+	const handleBookmarkClick = (movie: Movie) => {
+		const isBookmarked = bookmarkedMovies.some(
+			(bookmarkedMovie) => bookmarkedMovie.imdbID === movie.imdbID
+		);
+		if (isBookmarked) {
+			removeBookmarkedMovie(movie);
+		} else {
+			addBookmarkedMovie(movie);
+		}
+	};
+
+	const handleWatchedClick = (movie: Movie) => {
+		const isWatched = watchedMovies.some(
+			(watchedMovie) => watchedMovie.imdbID === movie.imdbID
+		);
+		if (isWatched) {
+			removeWatchedMovie(movie);
+		} else {
+			addWatchedMovie(movie);
+		}
+	};
+
+	return (
+		<Stack
+			as={Box}
+			textAlign={"center"}
+			spacing={{ base: 8, md: 14 }}
+			py={{ base: 20, md: 36 }}
+		>
+			<Heading
+				fontWeight={600}
+				fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
+				lineHeight={"110%"}
+			>
+				{watchedMovies.length > 0 ? (
+					<Text fontSize={25} fontFamily={"monospace"}>
+						Watched
+					</Text>
+				) : (
+					<Stack align={"center"}>
+						<Text fontSize={25} fontFamily={"monospace"}>
+							Hmm...doesn't look like you've watched anything yet.
+						</Text>
+						<Button
+							as={Link}
+							href='/'
+							colorScheme={"blue"}
+							bg={"blue.400"}
+							rounded={"full"}
+							px={6}
+							_hover={{
+								bg: "blue.500",
+							}}
+							type='submit'
+							w='50%'
+						>
+							Go to search
+						</Button>
+					</Stack>
+				)}
+			</Heading>
+			<ul
+				style={{
+					display: "flex",
+					flexDirection: "row",
+					width: "100%",
+					flexWrap: "wrap",
+					justifyContent: "center",
+				}}
+			>
+				{watchedMovies.map((movie) => (
+					<MovieCard
+						key={movie.imdbID}
+						movie={movie}
+						bookmarkedMovies={bookmarkedMovies}
+						watchedMovies={watchedMovies}
+						handleBookmarkClick={handleBookmarkClick}
+						handleWatchedClick={handleWatchedClick}
+					/>
+				))}
+			</ul>
+		</Stack>
+	);
+}
